e46c881c501eeefcc6a51889f11fdb5c34300d79
nasrullah-nobin <nasrullahbd82@gmail.com>
2026-02-05 23:45:35 +0600

    add function to perform subtraction, multiplication, and division on two numbers

---
+++ basic-math-operations.js
@@ -0,0 +1,13 @@
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+function divide(num1, num2) {
+  return num1 / num2;
+}
+console.log(subtract(23,56));
+console.log(multiply(23,56));
+console.log(divide(23,56));
+
