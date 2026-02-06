1138cc9d2516b7b336757b1bef68fbf28fa61b94
nasrullah-nobin <nasrullahbd82@gmail.com>
2026-02-06 07:54:40 +0600

    Create even and odd checker function

---
+++ oddOrEven.js
@@ -0,0 +1,10 @@
+function evenAndOddChecker(number) {
+  return number % 2 === 0 ? "Even" : "Odd";
+}
+console.log(evenAndOddChecker(5));
+
+// short Method
+function evnChecker(number) {
+  return number % 2 === 0;
+}
+console.log(evnChecker(5));
