078800e23776fe23a28cb50b7b66353df0fd574a
nasrullah-nobin <nasrullahbd82@gmail.com>
2026-02-06 16:46:45 +0600

    Implement string practice tasks: length, uppercase, word check, split, reverse, palindrome

---
+++ Strings (37–42)/string.js
@@ -0,0 +1,7 @@
+const str = "i love allah";
+console.log(str.length);
+console.log(str.toUpperCase());
+console.log(str.includes("love"));
+const riv = str.split("").reverse().join("");
+console.log(riv);
+console.log(str === riv)
