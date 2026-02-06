4efc0e6feff77b1f2e2fde5c3341e72d72330a70
nasrullah-nobin <nasrullahbd82@gmail.com>
2026-02-06 10:31:57 +0600

    Check types of different variables using typeof

---
+++ Variable & Data Type (9–13)/typeCheck.js
@@ -0,0 +1,19 @@
+const price = 50;
+const name = "Nobin";
+console.log(typeof price);
+console.log(typeof name);
+const equal = 5 == 5;
+console.log(typeof equal);
+const product = null;
+console.log(typeof product);
+const arr = [1, 2, 4, 5];
+console.log(typeof arr);
+const obj = {
+  name: "nobin",
+  age: 25,
+};
+console.log(typeof obj);
+let undi;
+console.log(typeof undi);
+const na = NaN;
+console.log(typeof na);
